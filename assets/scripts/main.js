926767673ed059bdf812271875d5110488217271
Max Oliveira <max-oliveira@outlook.com.br>
2022-05-28 21:41:19 -0300

    Terminando versão mobile

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,29 @@
-function onScroll() {
+window.addEventListener('scroll',onScroll)
 
-    if (scrollY > 0) {
-        navigation.classList.add('scroll');
-    } else {
-        navigation.classList.remove('scroll');
-    }
+onScroll();
+function onScroll() {
+    showNavOnScroll()
+showBackToTopButtonScroll()
 }
 
 function openMenu() {
     document.body.classList.add('menu-expanded')
+}
+
+function showNavOnScroll() {
+    if (scrollY > 0) {
+        document.querySelector("#navigation").classList.add('scroll');
+    } else {
+        document.querySelector("#navigation").classList.remove('scroll');
+    }
+}
+
+function showBackToTopButtonScroll() {
+    if (scrollY > 550) {
+        document.querySelector("#backToTopButton").classList.add('show');
+    } else {
+        document.querySelector("#backToTopButton").classList.remove('show');
+    }
 }
 
 function closeMenu() {
@@ -28,6 +43,6 @@ nav,
 #services header,
 #services .card,
 $about,
+#about header,
 #about .content,
-#about 
 `);
